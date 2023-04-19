ab095233332bdff1341f83ec04561701d3009c7d
Dmitriy <sirdima2014@gmail.com>
2023-04-19 17:00:20 +0300

    + reworked mycomputer info (write in binary file)

--- new/Network.py
+++ new/Network.py
@@ -73,7 +73,7 @@ class Network:
         if self.OS == "Windows":
             addr = subprocess.check_output("arp -a", shell=True, encoding="cp1251")
             ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
-            addr = re.findall(ip_regex, addr)
+            addr = set(re.findall(ip_regex, addr))
             self.addr = addr
 
     # перед работой сделать udp запросы по адресам, которые есть в self.addr
@@ -157,13 +157,13 @@ class Network:
             self.SendBot(bot=bot, data=data, port=self.port)
             if self.large is True:
                 for j in packets:
-                    print(bot,)
+                    print(bot, )
                     self.SendBot(bot=bot, data=j, port=self.reserved_port)
-
 
     # обработка расределённых действий
     def AcceptingAction(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         server_socket.bind(('0.0.0.0', self.port))
         while True:
@@ -199,7 +199,7 @@ class Network:
         if self.large is True:
             print("Custom TCP: ", self.large)
             StartParallels_threading.start()
-            # StartParallels_threading.join()  # True
+            StartParallels_threading.join()  # True
             print("Packets sends!")
             AcceptingAction_threading.start()
             # AcceptingAction_threading.join()
@@ -283,7 +283,7 @@ class Network:
                     print(subarrays)
                     self.StartAction(action=action, array=subarrays)
                     print("Standard sort", end - start)
-                    self.ResetAction()
+                    # self.ResetAction()
                 case _:
                     print("Command not found!")
 
